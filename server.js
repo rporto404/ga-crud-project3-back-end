37c096bbee305db56a72c86eae78c42d17b51ed3
PORTORREAL <portorreal@ad25wv020h7jy.myfiosgateway.com>
2022-05-27 17:38:53 -0400

    sending hello

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get('/', (req,res) => {
+  res.send('Hello World!');
+});
+
 app.listen(3000, () => {
   console.log('listening...');
 });
