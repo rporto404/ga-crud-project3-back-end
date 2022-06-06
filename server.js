cad7aaed60980681e043391c501866f04d746355
Brandon Feltz <elloimbrandon@gmail.com>
2022-06-05 17:49:57 -0700

    changing mongouri to heroku

--- server.js
+++ server.js
@@ -16,30 +16,30 @@ const PORT = process.env.PORT || 3000;
 const db = mongoose.connection;
 
 // connect to the database either via heroku/atlas
-// const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = process.env.MONGODB_URI;
 
 // local
-const mongoURI = "mongodb://0.0.0.0:27017/store";
+// const mongoURI = "mongodb://0.0.0.0:27017/store";
 
 // Connect to Mongo atlas
-// mongoose.connect(MONGODB_URI, () => {
-//   console.log("connected to mongo");
-// });
-
-// connect local
-mongoose.connect(mongoURI, () => {
+mongoose.connect(MONGODB_URI, () => {
   console.log("connected to mongo");
 });
 
+// connect local
+// mongoose.connect(mongoURI, () => {
+//   console.log("connected to mongo");
+// });
+
 // Error / success / mongo atlas
-// db.on("error", (err) => console.log(err.message + " is Mongod not running?"));
-// db.on("connected", () => console.log("mongo connected: ", MONGODB_URI));
-// db.on("disconnected", () => console.log("mongo disconnected"));
+db.on("error", (err) => console.log(err.message + " is Mongod not running?"));
+db.on("connected", () => console.log("mongo connected: ", MONGODB_URI));
+db.on("disconnected", () => console.log("mongo disconnected"));
 
 // Error / success / mongo local
-db.on("error", (err) => console.log(err.message + " is Mongod not running?"));
-db.on("connected", () => console.log("mongo connected: ", mongoURI));
-db.on("disconnected", () => console.log("mongo disconnected"));
+// db.on("error", (err) => console.log(err.message + " is Mongod not running?"));
+// db.on("connected", () => console.log("mongo connected: ", mongoURI));
+// db.on("disconnected", () => console.log("mongo disconnected"));
 
 // makes a /store default route name
 app.use("/store", storeController);
